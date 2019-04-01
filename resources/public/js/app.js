ea754d447fdc181eab0d7f4900e94269f06f4cf0
Sean Walker <swlkr.rbl@gmail.com>
2019-03-31 21:18:38 -0600

    Don't highlight menu items

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -12,7 +12,7 @@ $(function() {
   $('a[href="' + window.location.pathname + '"]').each(function(el) {
     if(window.location.pathname === '/docs') {
       $('a[href="/docs/installation.md"]').css('color', '#357EDD');
-    } else {
+    } else if (window.location.pathname.startsWith('/docs')) {
       $(this).css('color', '#357EDD');
     }
   })
